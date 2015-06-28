72fc1ccffe15f54f9f58927d756c6e7cfaf8266b
jfetter <jillian.fetter@gmail.com>
2015-06-27 17:48:30 -0700

    PalindromeMap.js seems to work going to see if it should be refactored

--- PalindromeMap.js
+++ PalindromeMap.js
@@ -26,3 +26,56 @@
 // (7)have the function look at each word individually to see if the word[0] = word[length] if they are equal, then move on to word[1] the word[length-1] ... keep going until word[a]=word[b]
 // (8) return true if all values equate until you reach the middle of the word. 
 //  planning thus far approx 20 min. 
+
+
+
+
+
+
+checkForPalindromism = function(letters){
+ 	var wordLength = letters.length -1; 
+ 	var palindrome = false; 
+ 	var matchCounter =0;
+ 	letters.map(function(all, item, index){
+ 	console.log(letters[item], letters[wordLength -item]);
+ 	if (letters[item] === (letters[wordLength - item] )){
+ 		matchCounter += 1;} // if letters ===
+ 	if (matchCounter >= letters.length){
+ 		palindrome = true;} //if matchCounter
+ 		},0); // map
+ 		console.log(palindrome);
+ 		return palindrome;
+ } //checkForPalindromism
+ 		// for : 12345
+ 		// 1 vs 5
+ 		// 2 vs 4
+ 		// 3 vs 3
+ 		// 4 vs 2
+ 		// 5 vs 1
+
+isItAPalindrome = function(text){
+ 	var trueFalseList = [];
+	var words = text.split(" ");
+ 	console.log(words);
+	return words.map(function(all, item, index){
+ 	  	var letters = (words[item]).split("");
+ 		console.log(letters);
+		if (checkForPalindromism(letters) === true)
+ 		{
+ 		trueFalseList.push(true);
+ 	} else { 
+ 	trueFalseList.push(false);
+ 	} 
+console.log(trueFalseList);
+return trueFalseList;
+ 	}); //map
+}// function
+
+// document.getElementById("outPut").innerHTML = isItAPalindrome(prompt("entersometext"));
+
+
+console.log(isItAPalindrome("aaaaa abc"));// true false
+console.log(isItAPalindrome("aba bbbb abc"));// false false false
+console.log(isItAPalindrome("72"));// false
+console.log(isItAPalindrome("101"));//true
+console.log(isItAPalindrome("a"));//true
