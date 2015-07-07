e463c241c44bd45be6a11faad9cf67fc5b697454
jfetter <jillian.fetter@gmail.com>
2015-07-06 19:44:01 -0700

    dates.js  more organized procedure but still not working 100%

--- dates.js
+++ dates.js
@@ -17,34 +17,41 @@
 //3. 
 
 
-
-var getDate = function(){
-	var year = prompt ("enter target year in the format #### eg 2015");
- 	var month =  prompt ("enter month to expire in the format 1-12");
- 	var day = prompt ("enter day to expire in the format 1-31");
- 	var futureDate = new Date(year, month -1, day);
- 	return futureDate; 
+//create an object breaking down the future day month and year, current date and new date. 
+var Until = { //probably need to set this up so this will run like a function; 
+	year: prompt ("enter target year in the format #### eg 2015"),
+ 	month:  prompt ("enter month to expire in the format 1-12"),
+ 	day: prompt ("enter day to expire in the format 1-31"),
+ 	futureDate: new Date(year, month -1, day),
+    now: new Date('April 2, 2015 0:00:00')
 };
 
-
-
-var Until = {
-futureDay: (getDate()),
-now: new Date('April 2, 2015 0:00:00')
-};
-
-console.log(Until.futureDay);
+console.log(Until.futureDate);
 console.log(Until.now);
 
+//determine the number of milliseconds that will ellapse
+var numbOfMilliseconds = function(futureDate, now){
+  var milliseconds = parseInt(future - now); 
+  console.log(milliseconds);
+  return milliseconds; }; // call with Until.futureDate, Until.Now
 
-// var futureMonth= function(futureDay){
-// var futureMonth = parseInt(Until.futureDay.getMonth())
-//   console.log(futureMonth);
-// if (futureMonth >= 4)
-//   {return futureMonth + 8;} 
-// else
-// {return futureMonth;}
-// };  
+//detrmine the number of years...
+var numbOfYears = function (milliseconds){
+  var years= milliseconds/31557600000; //31557600000 = 365.25 days to account for leap years. next leap day is 4.29.16
+  console.log(years);
+  return years; }; //call with numbOfMilliseconds-Until.futureDate, Until.now
+
+//determine the number of months... 
+var numbOfMonths= function(month, day){
+//var futureMonth = parseInt(Until.futureDay.getMonth())
+   var numberOfMonths;
+   if (month <= 4) {
+    numberOfMonths = 8+ month} 
+   else {numberOfMonths = month -4}; 
+   if (day >= 2) {numberOfMonths --};
+   console.log("Num Of Months:" + numberOfMonths);
+   return numberOfMonths; // NEED TO ACCOUNT FOR THERE BEING LESS THAN A FULL MONTH BEFORE 2nd AND MORE THAN A FULL MONTH AFTER THE 2nd. 
+}; // call with Until.month and Until.day 
 
 
 // var futureYears = function(futureDay){ 
@@ -56,44 +63,64 @@ console.log(Until.now);
 // };
 
 
-var calcMonthsAndDays = function(days, years){
-var days = days; 
-if (days <= 28) {
+// For all, do days – years…
+
+// Month = someNumber
+// //28 days left in April
+// if (days < 28), days = days. 
+// //?? last box in array is 1? –for date if it is april 1. ??
+
+// Else
+// {
+// factor in startPoint from the actual month they are starting from.
+// var monthToStartFrom = THE GIVEN MONTH OF THE END DATE. 
+// var monthDays = [days in month starting from January—but just 1 day for April…]
+// monthDays.reduce(function(days){})…
+// while days > days[i]
+// {days -= days[i]
+// month ++ 
+// var monthAndDay = [month, days]} //end while loop
+// }, MonthToStartFrom); // end reduce
+// } //end else
+// return monthAndDay;  
+
+var numbOfDays = function(milliseconds, years, months){
+  milliseconds = numbOfMilliseconds(Until.futureDate, Until.now);
+  var days = milliseconds/ parseInt(milliseconds/86400000); // 86400000 milliseconds in a day 
+  //subtract the full years out of the total number of days
+  days = days - ((365 * years) + parseInt((years +1)/4)); //subtract an extra day off in leap years 
+
+  if (days <= 28) { //since April 2nd is start day anything less than 28 days will fall in April, so will not need add' days dedcuted from total 
   days = days}
-else 
- {days = Math.abs(days - (365 * years) + parseInt((years +1)/4)); //subtract an extra day off in leap years 
-//futureMonth(Until.futureDay);
-
-//array of days in month starting from May
-var futureDays = [31, 30, 31, 31, 30, 31, 30, 31, 31, 28, 31, 30].reduce(function(all, item ,index, days, futureMonth){
-var months = 0; 
-while (days >= 0){
- days - index
-  console.log(all)
-  months ++;  };//while
-  return [all, months]; 
-},0); // reduce
-
+  else 
+  { //array of days in month starting from March and going backwards to May --April not included since it is either going to be one full year or less than one month if futureDate is in April.// go through subtracting days out until you get to May 
+  var futureDays = [31, 28, 31, 31, 30, 31, 31, 31, 31, 30, 31 ].reduce(function(all, item ,index){
+  //will need to know the month to begin in the array
+  var futureMonth = futureMonth(Until.month, Until.Day);
+  var days = days;//from earlier in CalcMonthsAndDays  
+  days - index;
+  months ++;
+  console.log(all, months);
+  return Math.abs(all);  //you will end up with a negative number when you get to the end. 
+}, (11 - numbOfMonths(futureDay))); // end reduce
+//subtract
 }//else
 return futureDays;
 } //calcMonthsAndDays
+//call numbOfDays with 
 
 
 var timeUntil = function (future, now){
+     
 
- var milliseconds = parseInt(future - now); 
-console.log(milliseconds);
-//  var seconds= parseInt(milliseconds/1000);       
- var days= parseInt(milliseconds/86400000); // 86400000 milliseconds in a day
- var years= milliseconds/31557600000; //31557600000 = 365.25 days to account for leap years
-// next leap day is 4.29.16
+
 
 calcMonthsAndDays(days, years); 
 
 // need to pull days, months out of the value returned from function
 
-
- 	if (days == 1){
+var displayYearMoDay = function()
+{ 	if (days == 1){
  	day = "1 day"}
  	else {days = days + " days"};
  	if (months >= 1 && months <= 2){
@@ -110,12 +137,11 @@ calcMonthsAndDays(days, years);
   var yearMoDay = [years, months, days].join("");
 
 return yearMoDay; 
-};// object TargetDate
+};// end object TargetDate
+} // end displayYearMoDay
 console.log(timeUntil(Until.futureDay, Until.now));
 
 
-//  	var myspan = document.getElementById("timer");  //get a handle on the <span> element 
-//  	myspan.innerHTML = timeUntil;					//set the content that displays between <span> and </span>
 
 	
 
