34807be9b95657a68ad0201301e537e2a8fba93e
jfetter <jillian.fetter@gmail.com>
2015-07-09 20:49:22 -0700

    can change the start date now but still having trouble with the past

--- dates.js
+++ dates.js
@@ -14,78 +14,111 @@
 
 
 //create an object that aquires and assigns variables to the new day month and year 
-var Between = {
+var Then = {
     year: parseInt(prompt("enter target year in the format #### eg 2015")),
     month: parseInt(prompt("enter month to expire in the format 1-12")),
-    day: parseInt(prompt("enter day to expire in the format 1-31")),
+    day: parseInt(prompt("enter day to expire in the format 1-31"))
 };
 
-console.log(Between.year, Between.month, Between.day);
+//create an object for the current date
+var Today ={
+    year: parseInt(2015),
+    month: parseInt(7),
+    day: parseInt(9)
+};
+
+console.log(Then.year, Then.month, Then.day);
+console.log(Today.year, Today.month, Today.day);
 //create an object for today and the new day
 var Dates = {
-    then: new Date(Between.year, Between.month, Between.day),
-    today: new Date('April 2, 2015 0:00:00')
+    then: new Date(Then.year, Then.month -1, Then.day),
+    today: new Date(Today.year, Today.month -1, Today.day)
 };
 
-//determine the number of milliseconds that will ellapse
-var numbOfMilliseconds = function(then, today) {
-    var milliseconds = then - today;
+//determine the number of milliseconds that will ellapse between Then and Today
+var numbOfMilliseconds = function() {
+    var milliseconds = Dates.then - Dates.today;
     console.log(milliseconds);
     return milliseconds;
 };
 
-//detrmine the number of years...
-var numbOfYears = function(milliseconds) {
-    var years = (milliseconds / 31557600000)  ; //31557600000 = 365.25 days ...adding extra 1/4day to account for leap years 
-    return years;
-};
+
+
 
 //determine the number of months... 
-var numbOfMonths = function(month, days) {
-    var months = parseInt(month)- 4; // determine the distance from April 
-    console.log("Num Of Months:" + months);
-    return months; //if the month is before April, it will be a negative value
-};
+// var numbOfMonths = function() {
+//     var months;// determine the distance the given month is from today 
+//     if (Today.month <= Then.month) { //
+//     months = Then.month - Today.month; 
+//         }//end if the numberic value of today is less than the value of then
+//     else {months = Today.month + Then.month}; // end if the value of today is less than then 
+//     console.log("Num Of Months:" + months);
+//     return months; 
+// };
+
+//calculate the number of years
+var numbOfYears = function(){
+    var years = parseInt(Math.abs((numbOfMilliseconds()/ 31536000000))); //31536000000 = number of milliseconds in a year  
+    console.log ("years" + years);
+    if (years > 1){parseInt(Math.abs((numbOfMilliseconds()/ 31536000000/31557600000))); //31557600000 = number of milliseconds in 365.25 days -- to account for leap year
+    }
+    return years; 
+ };
 
 //make the returns from timeframes easily accessible for display and calculation of days
 var Count = {
-    milliseconds: numbOfMilliseconds(Dates.then, Dates.today),
-    days: parseInt(numbOfMilliseconds(Dates.then, Dates.today) / 86459178.1), // 86400000 milliseconds in a day + 0.0006849315 of a day add'l milliseconds
-    years: parseInt(numbOfYears(numbOfMilliseconds(Dates.then, Dates.today))), 
-    months: parseInt(numbOfMonths(Between.month, Between.day))
+    milliseconds: numbOfMilliseconds(),
+    days: Math.abs(parseInt(numbOfMilliseconds() /86400000 )), // 86400000 milliseconds in a day 
+    years: numbOfYears(),
+   // months: numbOfMonths()
 };
 
-//calculate the number of days remaining after years and months are subtracted
-var numbOfDays = function(days, years, months) {
-    //subtract the full years out of the total number of days 
-    console.log(days);
-    days = days - ((365 * years) + ((years + 3) / 4)); // subtract an extra day off for leap years we are 3/4 of way to next leap year
-    console.log("start days: " + days);
-        console.log (Between.month);
-        //array of days in month starting from January
-        var daysInMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-        if (days < 0){ //if the new date is in the past
-            for (var i = Between.month -2 ; i >= 0; i--) //subtract one from the month because array starts at zero and subtract another one because you are looking at days up until, but not including this month
-            {days += daysInMonths[i]; 
-            console.log(i, Between.month, days);
-            }// end for
-            }//end if
-        else { //if the new date is in the future...
-        for (var i = 11 - Count.months; i < daysInMonths.length; i++) 
-        {
-            days -= daysInMonths[i]; //subtract the number of days in each month starting from the number of months back in the array
-            console.log(i, Count.months, days);
-        } //end for
-        }// end else
-        days = Math.abs(days); // will end up with a negative number when at the end of the array. 
-   // } //end else
-    
-    return days;
-} //end numbOfDays
 
-var displayYearMoDay = function(months, years) 
+//calculate the number of days remaining after years and months are subtracted
+var numbOfDays = function() {
+    //subtract the full years out of the total number of days 
+    console.log("days before years deducted" + Count.days);
+    console.log("years: "+ Count.years);
+    days = parseInt((Count.days - (365 * Count.years)) - ((Count.years -(Then.year%4))/ 4));//subtract out an extra day for each leap day that has passed since current year and subtract out the remainder of years since last leap year  
+    console.log("start days: " + days);
+
+        if (( Then.months < 2 ||(Then.months == 2 && days <= 28) )){ //if its passed february assess wether its a leap year 
+            if (((Count.years + 3) % 4) == 0) { // in 2015 we are 3/4 of the way to the next leap year
+                days += 1; // if it is a leap year,but leap year hasn't come yet, put back that extra day
+            } // end if leap years
+        }// end if after Feb 28s
+
+
+        //array of days in month starting from January
+        var daysInMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]; 
+        var i = Today.month-1;
+        var countOfMonths = 0;
+        while (days > daysInMonths[i]) //subtract days in past months until you have less days than the current month 
+        {
+            if (Today.month <= Then.month ||( Count.milliseconds <0 && Today.month >= Then.month) ){
+            days -= daysInMonths[i]; 
+            i++;
+            
+            if(i > 11){i-=11};          //if you reach the end of the array loop back to the beginning
+             } //if Today.month < 
+            else {
+            days -= daysInMonths[i];  
+            i--;
+            if(i < 0){i+=11};
+
+            }; // else Today.month >
+          console.log(i, daysInMonths[i], days);
+        } //end while    
+        console.log
+       if (Count.milliseconds < 0){ days -= daysInMonths[Then.month-1]}; //account for the fact that you are moving backwards in time not forwards
+
+    return [Math.abs(days), countOfMonths];
+}//end numbOfDays  
+
+var displayYearMoDay = function() 
 {
-    var days = numbOfDays(Count.days, Count.years, Count.months);
+    var days = numbOfDays();
+    var days = days[0];
     console.log("days I end up with " + days)
     if (days == 1) {
         days = "1 day"
@@ -93,7 +126,7 @@ var displayYearMoDay = function(months, years)
     else {
         days = days + " days"
     };
-    months = Math.abs(months);
+    months = days[1];
     if (months == 1) {
         months = "1 month,  "
     } 
@@ -103,21 +136,21 @@ var displayYearMoDay = function(months, years)
     else {
         months = months + " months, "
     };
-    if (years == 1) {
+    if (Count.years == 1) {
         years = "1 year,"
     } 
-    else if (years == 0) {
+    else if (Count.years == 0) {
         years = ""
     } 
     else {
-        years = years + " years, "
+        years = Count.years + " years, "
     }
     ;
     var yearMoDay = [years, months, days].join("");
     
     return yearMoDay;
 }; // end displayYearMoDay
-console.log(displayYearMoDay(Count.months, Count.years));
+console.log(displayYearMoDay());
 
 
 
