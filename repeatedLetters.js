14262e5f2df9ca98867b348f3cc9e20ab9f9c9a6
jfetter <jillian.fetter@gmail.com>
2015-07-22 13:57:16 -0700

    rough draft of repeat letters doesn't run yet

--- repeatedLetters.js
+++ repeatedLetters.js
@@ -9,15 +9,38 @@
 
 // ["attribute", "success"]
 
-//create a variable that is a string-- test cases for: var string = "woo me my boo"; "He was an eerie bookeeper"; "I would have gone to mississippi, but at the time I was willless"; "I attribute my success to this: I never gave or took any excuse. –Florence Nightingale"
-//write a function to divide the string in to words: var divde = function(string){string.split()};
-// create a variable that is a counter var currentCountOfRepeats = 0; 
-// create an array to compare for the previous word var oldCountOfRepeats = [];
-// push the word to an array
-// change all letters to be lowercase
-// sort the letters in each word so that they are arranged in alphabetical order.
-// loop through the sorted letters in the world 
-// if ([i]==[i+1] ) {countOfRepeats ++};
-// push the value on the counter to an array... 
-// compare the value in the counter array to the value of the current counter.  if the value is greater than the previous value pop the previous word from the word array and pop the value from the counter array then push the new values to each array 
-// if the value of the counter is equal then just push the values to the arrays 
+var wordSelector = function(string){
+	// create a variable that is a counter var currentCountOfRepeats = 0; 
+ 	var currentCountOfRepeats = 0; 
+ 	// create an array to compare for the previous word var oldCountOfRepeats = [];
+ 	var oldCountOfRepeats = 0;
+ 	//createa an array to store the word with the most repeats-- start it out with something to avoid an error when the first word goes in;
+ 	var wordWithMostRepeats =["words will replace this"];
+	//  divide the string in to an array  of word
+	var arrayOfWords = string.split(" "); 
+ 	var searchForRepeats = arrayOfWords.map(function(all, item, index){
+ 		// change all letters to be lowercase and sort the letters in each word so that they are arranged in alphabetical order.
+ 	 	var sortedWord = (item.lowercase).sort(); 
+ 		// divide the word into letters so they can be compared
+ 		var letters = sortedWord.spilt(""); 
+ 		// if the current letter is the same as the next letter, add 1 to a counter of repeated letters
+ 		while (item < letters.length -1){
+ 		if (letters[index] == letters[index + 1]){
+ 			currentCountOfRepeats ++;
+ 		};
+ 		// if there are more repeat letters in the current word (or current sequence in the same word) remove the old word and add the new word to the most repeats array;
+ 		if (oldCountOfRepeats < currentCountOfRepeats){
+ 			wordWithMostRepeats.pop()}
+ 		if (oldCountOfRepeats <= currentCountOfRepeats){
+ 			oldCountOfRepeats = currentCountOfRepeats;  
+ 			wordWithMostRepeats.push(arrayOfWords[item])
+ 		}; // end if oldCountOfRepeats...
+ 	}; // end while 
+ 	return wordWithMostRepeats; 	
+ 	},0);
+ 	return wordWithMostRepeats;
+};
+
+console.log(wordSelector("He was an eerie bookeeper")); // returns [eerie, bookeeper] 
+console.log(wordSelector("I would have gone to mississippi, but I was willless")); // returns [mississippi] oorrr Mississipi???
+console.log(wordSelector("I attribute my success to this: I never gave or took any excuse. –Florence Nightingale")); // returns [attribute, success] 
