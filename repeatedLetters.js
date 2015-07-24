052bc1f9d473522f139fbae03feb87162a36fc2e
jfetter <jillian.fetter@gmail.com>
2015-07-24 06:54:00 -0700

    almost working just need to fix return on searchForRepeats function

--- repeatedLetters.js
+++ repeatedLetters.js
@@ -10,37 +10,56 @@
 // ["attribute", "success"]
 
 var wordSelector = function(string){
-	// create a variable that is a counter var currentCountOfRepeats = 0; 
- 	var currentCountOfRepeats = 0; 
  	// create an array to compare for the previous word var oldCountOfRepeats = [];
  	var oldCountOfRepeats = 0;
- 	//createa an array to store the word with the most repeats-- start it out with something to avoid an error when the first word goes in;
- 	var wordWithMostRepeats =["words will replace this"];
+ 	//create an array to store the word with the most repeats-- start it out with something to avoid an error when the first word goes in;
+ 	var wordWithMostRepeats =[];
 	//  divide the string in to an array  of word
 	var arrayOfWords = string.split(" "); 
- 	var searchForRepeats = arrayOfWords.map(function(all, item, index){
- 		// change all letters to be lowercase and sort the letters in each word so that they are arranged in alphabetical order.
- 	 	var sortedWord = (item.lowercase).sort(); 
+ 	var searchForRepeats = function(){
+         arrayOfWords.map(function(item, index, all){
+
  		// divide the word into letters so they can be compared
- 		var letters = sortedWord.spilt(""); 
- 		// if the current letter is the same as the next letter, add 1 to a counter of repeated letters
- 		while (item < letters.length -1){
- 		if (letters[index] == letters[index + 1]){
- 			currentCountOfRepeats ++;
- 		};
- 		// if there are more repeat letters in the current word (or current sequence in the same word) remove the old word and add the new word to the most repeats array;
- 		if (oldCountOfRepeats < currentCountOfRepeats){
- 			wordWithMostRepeats.pop()}
- 		if (oldCountOfRepeats <= currentCountOfRepeats){
- 			oldCountOfRepeats = currentCountOfRepeats;  
- 			wordWithMostRepeats.push(arrayOfWords[item])
- 		}; // end if oldCountOfRepeats...
- 	}; // end while 
- 	return wordWithMostRepeats; 	
- 	},0);
- 	return wordWithMostRepeats;
-};
+		var letters = item.toLowerCase().split(""); 
+ 		// change all letters to be lowercase and sort the letters in each word so that they are arranged in alphabetical order.
+  	 	console.log(letters);
+ 		var sortedWord = letters.sort();
+ 		console.log(sortedWord); 		
+ 		var currentCountOfRepeats =0;
+	  	console.log(currentCountOfRepeats);
+ 		sortedWord.map(function(item1, index1, all1){
+ 			//if the current letter is the same as the next letter, add 1 to a counter of repeated letters 
+  			if (sortedWord[index1] === sortedWord[index1 +1]){
+  			currentCountOfRepeats ++;
+  			}		
+  			//reset currentCounteOfRepeats when number of repeat letters ceaeses to grow.(to prevent multiple sets of repeat pais in the same word from being added to counter for that word)
+ 			else
+			{currentCountOfRepeats = 0}
+ 			// if there are more repeat letters remove the old word and add the new word to the most repeats array;
+   			if (currentCountOfRepeats > oldCountOfRepeats){
+  			wordWithMostRepeats = [];
+ 			oldCountOfRepeats = currentCountOfRepeats; 
+ 			console.log(oldCountOfRepeats, wordWithMostRepeats);
+  			} // end if oldCountOfRepeats...
+ 			// if there are more or an equal amount of consecutive letters than any word before push this word to the wordsWithMostRepeats array
+  			if (currentCountOfRepeats >= oldCountOfRepeats){
+ 				//prevent words that have already been added to wordWithMostRepeats from being re-added; 
+  			 	if (wordWithMostRepeats[wordWithMostRepeats.length - 1] != arrayOfWords[index])
+ 				{wordWithMostRepeats.push(arrayOfWords[index])
+ 				}; // end avoid repeats in array
+ 			} //end if <=
+ 				return wordWithMostRepeats;
+  			},0); // end map sortedWord
+  			 	console.log("final" + wordWithMostRepeats);
+ 			return wordWithMostRepeats;
+ 		},0); // end map arrayOfWords;
+ 	};
+//  	var repeatOffender = document.getElementById("repeatOffender");  //get a handle on the <div> element 
+// 	repeatOffender.innerHTML = wordWithMostRepeats;					//set the content that displays in the <div>
+ 	return searchForRepeats();
+}; // end wordSelector
 
 console.log(wordSelector("He was an eerie bookeeper")); // returns [eerie, bookeeper] 
 console.log(wordSelector("I would have gone to mississippi, but I was willless")); // returns [mississippi] oorrr Mississipi???
 console.log(wordSelector("I attribute my success to this: I never gave or took any excuse. –Florence Nightingale")); // returns [attribute, success] 
+console.log(wordSelector("each word has no duplicates"));
