c77867aec63f12361e87f16b5b5221787124c259
jfetter <jillian.fetter@gmail.com>
2015-08-11 19:04:56 -0700

    added pictures to widget mock up and rough start to js code

---
+++ 16WidgetMockUp/localJS.js
@@ -0,0 +1,12 @@
+var product = function (quantity, price){
+ quantity * price // get the price from the defined field of text
+ 
+
+};
+
+//plug in variables from a class
+
+function merch (){
+	this.price = getElementByID // price
+	this.quantity = getElementByID // quantity
+}
