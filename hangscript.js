0d346a4644d7ddc8c264d3ec0972612e3a8b1b4e
jfetter <jillian.fetter@gmail.com>
2015-08-26 18:32:54 -0700

    adjusted rows and colums in mock up and added more planing in hangman

--- hangscript.js
+++ hangscript.js
@@ -1,13 +1,7 @@
-
-// set up 2 html pages to switch between. One pages is the accusor - explains rules gives game dialogue etc.
-// 2nd page is the actual hang man gallows with stick figure walking towards platform and display of
-// blanks spaces, used letters, available letters. 
 // 1. function to ammend available letters based on key entered-> replace available letters with "". 
 // include a loop to denote invalid key entrered (for numbers, symbols etc)
 // 2. function display used letters -> when letters are moved out of available they are pushed into an array either for used letters (which are not in the answer) OR the letters are revealed in the answer; 
-//  	var e = jQuery.Event( 'keydown', { which: $.ui.keyCode.ENTER } );
-// $('input').trigger(e);ascii.map(function (item, index, all){return String.fromCharCode(item); },0).toString().replace(/,/g,"")
-// 2a. array of ascii table so letters correspond to ascii positions ()
+// 2a. array of ascii table so letters correspond to ascii positions (ascii.map(function (item, index, all){return String.fromCharCode(item); },0).toString().replace(/,/g,""))
 // 2b. function to display the appropriate number of blank spaces at the beginning of the game based on the random word selected
 // 3. function to make the screen flashed when a used letter is attempted to be used
 // 4. function to build the hanging man. 
@@ -25,29 +19,16 @@
 // 9. function to advance dialogue in response to key entry
 // 10. function to determine if the game is won/ lost (counter to track every time a word is added to the used word bank)
 // . function to animate win/ loss (from a library) --> Have dialogue character run back and forth across the screen with a comment bubble that says "Yay" when you win and runs off the screen saying "uh-Oh" when you lose
-//    $("img").animate({ top: '+=100px'}, 1000);
 
-// $(document).ready(function(item){
+
 
 $(document).ready(function(){
-$("#usedLetters").keyup(function(){
-    var usedLetter = $("#usedLetters").val().toLowerCase();
+$("#username").keyup(function(){
+    var username_value = $("#username").val().toLowerCase();
         availableLets = [];
-    if (availableLets.indexOf(user) > -1){
-     $("#usedLetters ").append("<span>usedLetter</span>");
-}
+    //if (username_value.isContainedIn(usedLetters)==true){
+    // usedLetters.push(username_value);
+//}
     
 });
 });
-
-//at document ready (execute this section of code only once (1) function to randomly select a word from the word array
-//(2) function to break the word into an array of letters .split("")
-//(3)function to set up a number of blanks that corresponds to the .length of the array
-
-// coordinate so (1)dilogue says choose a letter!
-//(2) image changes to gallows and character walking towards gallows $("#picture").attr("src","gallows.png")
-//(3) css changes id of input field for enterLetter from invisible to a visible color $("#enterLetter").css("display","inline-block");
-//(5) display box shwoing letters used
-// if a letter that is entered is contained in the used letter element OR the 
-// key typed is not a letter, then blink the screen .fadeIn("fast").delay(800)
-//    .fadeout("fast")
