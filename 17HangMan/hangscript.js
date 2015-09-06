f3f6da71df5c59b66508ae8c348f919eada892ec
jfetter <jillian.fetter@gmail.com>
2015-09-05 18:07:06 -0700

    takes letter input but still working on handing off the input letter to the function that will decide how to classify it

--- 17HangMan/hangscript.js
+++ 17HangMan/hangscript.js
@@ -1,31 +1,5 @@
 
 
-// 1. function to ammend available letters based on key entered-> replace available letters with "". 
-// include a loop to denote invalid key entrered (for numbers, symbols etc)
-// 2. function display used letters -> when letters are moved out of available they are pushed into an array either for used letters (which are not in the answer) OR the letters are revealed in the answer; 
-//  	var e = jQuery.Event( 'keydown', { which: $.ui.keyCode.ENTER } );
-// $('input').trigger(e);ascii.map(function (item, index, all){return String.fromCharCode(item); },0).toString().replace(/,/g,"")
-// 2a. array of ascii table so letters correspond to ascii positions ()
-// 2b. function to display the appropriate number of blank spaces at the beginning of the game based on the random word selected
-// 3. function to make the screen flashed when a used letter is attempted to be used
-// 4. function to build the hanging man. 
-// 5. array for word bank (divide into word length?/ difficulty?)
-// var words =["it","can","be","intimidating","to","leave","your","job","and","pursue","novel","career","opportunities","but","ultimately","when","given","only","one", "life", "experiencing", "more", ""]
-// if time create difficulty: 
-// var easty = words[0,1,2,5,];
-// var medium = words[];
-// var difficult = words [];
-// 6. randomly select word based on position in array (0-149);
-// 7. create a class called dialogue with (an) array(s) of instructions and comments that will appear throughout the game
-// 	var dialogue =["You may be wondering why you're here","You are being tried by the grammar police" , crimes(), "to prove your innocence you must correctly guess the word I am thinking of", "the punishment for failure...","is death","by hanging"]
-// var crimes = ["literally using the word literally figuratively", "making a run on a sentenced inmate that didn't desrve to be so bombarded by all of the things that you felt were deserved by him", "cow-tipping", "putting a comma in a coma", "unlawful use of you're your", "failure to complete your sentence...", "public indecent text poster", "alligations of alliteration for alluvial allocation"]
-// 8.function to choose a random crime to sentence the defendant to var crimes = function(crimes){return: " for" + (crimes[math.random()])}
-// 9. function to advance dialogue in response to key entry
-// 10. function to determine if the game is won/ lost (counter to track every time a word is added to the used word bank)
-// . function to animate win/ loss (from a library) --> Have dialogue character run back and forth across the screen with a comment bubble that says "Yay" when you win and runs off the screen saying "uh-Oh" when you lose
-//    $("img").animate({ top: '+=100px'}, 1000);
-
-
 
 $(document).ready(function(){
     
@@ -48,6 +22,8 @@ $(document).ready(function(){
         }  
     var spaces = spaceOut(letters.length);
 
+
+
     var lightening = function(){
       return $("body").fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
       //$('#sound_tag')[0].play();
@@ -62,24 +38,31 @@ $(document).ready(function(){
     }    
     $("#blanks").text(spaces); 
 
+var inputGuess = function(){
+    $("#enterLetter").focus();
+    $("#enterLetter").keydown(function(){ 
+    var guess = $("#enterLetter").val().toLowerCase();
+//hide the input letter field after a letter is entered space bar counts as first entry
+    if (guess.length >= 1){  $("#enterLetter, #storeLetter").addClass("invisible"); } 
+        $("#storeLetter").text(guess);
+    console.log("in function" + guess);
+    return guess;
+    }); //end keyup
+}   // end input guess 
+ 
 // see if the letter that was guessed is in the word and if so, plug it in to the display where the blanks are whever that letter occurs
-//      var checkGuess = function(letters){
-//      var letters = letters;
+      var checkGuess = function(guess){
+            console.log("then guess is =" + guess);
 //      letters.map.(function(all,item,index)
 //      var answerSoFar = $("#blanks").text().split(); //**** NEED TO TAKE INFO FROM BLANKS AND CONVERT IT INTO AN ARRAY 
 //      if ($("#enterLetter").text() === letters[index]){ 
-//        answersSoFar= answersSoFar.splice(index, 0, enterLetter))  } // insert the letter that was entered into the array in the place where the letter appears in the answer word
+//        answersSoFar= answersSoFar.splice(index, 0, enterLetter))  } 
+// insert the letter that was entered into the array in the place where the letter appears in the answer word
 //        answersSoFar= answersSoFar.splice(index -1, 1) // remove the blanks where the letter was placed
 //          )}
-//        return $("#blanks").text(answerSoFar.toString().replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()]/g,"");); //show the new display of blanks and letters in the answer -- without any commas or square brackets
-
-// grab only one letter that the user has inputinto the enterLetter input field
-//Only allow one letter to be input at a time (per turn).
-
-var inputGuess = function(letters){
-    var letters = letters; 
-    $("#enterLetter").keyup(function(){
-    var letterValue = $("#enterLetter").val().toLowerCase();
+//        return $("#blanks").text(answerSoFar.toString().replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()]/g,""););
+//show the new display of blanks and letters in the answer -- without any commas or square brackets 
+//grab one letter (only 1 letter per turn) that the user has inputinto the enterLetter input field
 
 // compare the letter that was input to see if it is an already used letter, if it is a new match with a letter from the word OR if it is a new wrong guess 
 
@@ -90,9 +73,9 @@ var inputGuess = function(letters){
 // }
 //     else {
 //     return letterValue }
-    
-});
-}
+} // end checkGuess    
+
+
 
 // end of game sequences
 var happyDance = function(){animation}
@@ -105,18 +88,17 @@ var winOrLose = function() {
 }
 
 // loop to control flow of game       
-    var gamePlay = function(letters){ 
-        var letters = letters;             
-        console.log(letters);
+    var gamePlay = function(){             
         Mousetrap.bind('space', function() { 
         $("#speech").addClass("invisible");
         $("#blanks, #enterLetter, #usedLetters").removeClass("invisible");
         var count = 0; 
+//      while (length <= 8)
         switchPic(count);
         var wrongLetters = []; 
         var length = wrongLetters.length;
-        inputGuess(); 
-//             while (length <= 8)
+        var guess = inputGuess();
+        checkGuess(guess); 
 //             {
 //           if (inputGuess() === "usedLetter"){lightening()};
 //           else { checkGuess() }// see if new letter is in the solution word 
@@ -146,41 +128,15 @@ var winOrLose = function() {
     sayNow = parseInt(sayNow); 
     console.log(sayNow + "now")
    if ( sayNow < dialogue.length ){          
-        return $("#speech").text(dialogue[sayNow])
+        $("#speech").text(dialogue[sayNow])
         }
     else {
         lightening(); 
         $("#speech").text("press space to begin");
-         return gamePlay();
+        gamePlay();
     }
 });
   
                 
 });
 
-//at document ready (execute this section of code only once (1) function to randomly select a word from the word array
-//(2) function to break the word into an array of letters .split("")
-//(3)function to set up a number of blanks that corresponds to the .length of the array
-//(4)roll intro dialogue from the accusor -- including randomly selected crime
-
-// for game play loop 
-// coordinate so (1)dilogue says choose a letter!
-//(2) image changes to gallows and character walking towards gallows $("#picture").attr("src",image[i]);
-//(3) css changes id of input field for enterLetter from invisible to a visible color $("#enterLetter").css("display","inline-block");
-//(5) display box showing letters used
-// // after prompting, take user input of one letter 
-// $("#usedLetters").keyup(function(){
-//     var usedLetter = $("#usedLetters").val().toLowerCase();
-//         availableLets = [];
-//     if (availableLets.indexOf(user) > -1){
-//      $("#usedLetters ").append("<span>usedLetter</span>");
-// }
-    
-// }); // end document ready
-
-// if a letter that is entered is contained in the used letter element OR the 
-// key typed is not a letter, then blink the screen .fadeIn("fast").delay(800)
-//    .fadeout("fast")
-// game play loop ends when either 9 wrong guesses are given OR all blanks are filled in
-
-//end of game either animation of stick figure swinging on noose OR image of accusor and dialoge like "my bad. Guess you do know a think or two about words. case dismissed"
